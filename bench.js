8bf8e8f75890a4f6b43bc3a7bf5b8934c92df150
Patrick Smith <pgwsmith@gmail.com>
2021-11-16 12:56:24 +1100

    Benchmark Math.max()

--- bench.js
+++ bench.js
@@ -6,6 +6,8 @@ for (let i = 0; i < 1000000; i++) {
     values.push(i);
 }
 
+const fewerValues = values.slice(0, 10000);
+
 function some(list, predicate) {
     if (list == null) {
         return false;
@@ -43,6 +45,12 @@ suite
     })
     .add('for of loop', () => {
         const processed = some(values, value => value > 990000);
+    })
+    .add('Math.max(...)', () => {
+        const processed = Math.max(...fewerValues);
+    })
+    .add('reduce(Math.max)', () => {
+        const processed = fewerValues.reduce((max, current) => Math.max(max, current));
     })
     .on('cycle', event => {
         const benchmark = event.target;
