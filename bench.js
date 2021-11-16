e22d6935f15757dc0ef38c3625286abbab61a499
Patrick Smith <pgwsmith@gmail.com>
2021-11-16 13:35:14 +1100

    Add more tests

--- bench.js
+++ bench.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 1000000; i++) {
 }
 
 const fewerValues = values.slice(0, 10000);
+const strings = values.map(n => n.toString());
 
 function some(list, predicate) {
     if (list == null) {
@@ -46,11 +47,45 @@ suite
     .add('for of loop', () => {
         const processed = someIter(values, value => value > 990000);
     })
+    .add('count evens with .filter().length', () => {
+        const processed = values.filter(n => n % 2 === 0).length;
+    })
+    .add('count evens with for i++ loop', () => {
+        let count = 0;
+        for (let i = 0; i < values.length; i++) {
+            if (values[i] % 2 === 0) {
+                count++;
+            }
+        }
+    })
+    .add('count evens with for of loop', () => {
+        let count = 0;
+        for (const n of values) {
+            if (n % 2 === 0) {
+                count++;
+            }
+        }
+    })
     .add('Math.max(...)', () => {
         const processed = Math.max(...fewerValues);
     })
     .add('reduce(Math.max)', () => {
         const processed = fewerValues.reduce((max, current) => Math.max(max, current));
+    })
+    .add('join strings with reduce(a + b)', () => {
+        const processed = strings.reduce((memo, current) => memo + current, '');
+    })
+    .add('join strings with .join()', () => {
+        const processed = strings.join('');
+    })
+    .add('join numbers with reduce(a + b)', () => {
+        const processed = values.reduce((memo, current) => memo + current.toString(), '');
+    })
+    .add('join numbers with .join()', () => {
+        const processed = values.join('');
+    })
+    .add('join numbers with .map().join()', () => {
+        const processed = values.map(n => n.toString()).join('');
     })
     .on('cycle', event => {
         const benchmark = event.target;
